a9d1cc7182aef95122d96f9eadcd64302ea3d6ec
JohnB116 <john.barden17@gmail.com>
2020-11-12 16:18:48 -0500

    Action Button and Dialog added to main activity

--- Fiscalitics/app/src/main/java/com/example/fiscalitics/MainActivity.java
+++ Fiscalitics/app/src/main/java/com/example/fiscalitics/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.fiscalitics;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean animate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,19 @@ public class MainActivity extends AppCompatActivity {
         */ //Just an example of adding a transaction to sharedpreferences with the class
            // I made, delete this at your liking
 
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animate = ViewAnimation.animateFab(fab, !animate);
 
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Money").setMessage("So, you like spending money, do ya?");
+                AlertDialog a = builder.create();
+                a.show();
+
+            }
+        });
 
 
     }
---
+++ Fiscalitics/app/src/main/java/com/example/fiscalitics/ViewAnimation.java
@@ -0,0 +1,18 @@
+package com.example.fiscalitics;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.view.View;
+
+public class ViewAnimation {
+
+    public static boolean animateFab(final View v, Boolean animate){
+        v.animate().setDuration(200).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+            }
+        });
+        return animate;
+    }
+}
