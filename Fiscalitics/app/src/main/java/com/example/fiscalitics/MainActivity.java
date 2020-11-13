588c82b9d3a04c38280f059e6bba1989c3a561eb
Dante Coupet <dacoupet@gmail.com>
2020-11-13 13:06:11 -0500

    files

--- Fiscalitics/app/src/main/java/com/example/fiscalitics/MainActivity.java
+++ Fiscalitics/app/src/main/java/com/example/fiscalitics/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.fiscalitics;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -14,10 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
     boolean animate = false;
 
+    /* using a TAG for logging. thought it would be useful for debugging ~_~ */
+    private static final String TAG = MainActivity.class.getCanonicalName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText etView = new EditText(this);
 
         /*
         int i = 1; //Arbitrary ID
@@ -38,15 +48,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 animate = ViewAnimation.animateFab(fab, !animate);
-
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Money").setMessage("So, you like spending money, do ya?");
+                builder.setTitle("Money").setMessage("So, you like spending money, do ya? Well STOP IT");
+                etView.setInputType(InputType.TYPE_CLASS_NUMBER);
+                
+                if(etView.getParent() != null) {
+                    ((ViewGroup)etView.getParent()).removeView(etView);
+                    Log.v(TAG,"removing the view");
+                }
+                builder.setView(etView);
+                Log.v(TAG,"new view created");
+                builder.setPositiveButton("+", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
                 AlertDialog a = builder.create();
                 a.show();
-
             }
         });
-
-
     }
 }
