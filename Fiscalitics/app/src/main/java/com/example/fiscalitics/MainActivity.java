f5e65dd573bfbd6c5bf4e108a673754de4589b02
JohnB116 <john.barden17@gmail.com>
2020-11-17 23:39:30 -0500

    added a new activity the user can access and a few more minor changes

--- Fiscalitics/app/src/main/java/com/example/fiscalitics/MainActivity.java
+++ Fiscalitics/app/src/main/java/com/example/fiscalitics/MainActivity.java
@@ -8,9 +8,12 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.InputType;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -22,11 +25,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class MainActivity extends AppCompatActivity {
 
     boolean animate = false;
+    float x1, x2, y1, y2;
 
     /* using a TAG for logging. thought it would be useful for debugging ~_~ */
     private static final String TAG = MainActivity.class.getCanonicalName();
-    private int id = 0;    //Add this to sharedpreferences eventually
-    private int count = 0; //count up items in storage
+    private int id = 0;      //Add this to sharedpreferences eventually
+    private int count = 0;   //count up items in storage
+    private float total = 0; //add up all money spent
     private String data = null;
 
     @Override
@@ -71,9 +76,12 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialogInterface, int i) {
 
 
-                        if (etView.getText().toString() != null) data
-                                = etView.getText().toString(); //getting the data entered into the view
-                        else return;                           //can't let them enter null!
+                        if (!etView.getText().toString().equals("")) {
+                            //getting the data entered into the view
+                            data = etView.getText().toString();
+                        }
+                        //can't let them enter null!
+                        else{return;}
                         Intent theIntent = new Intent();
 
                         //Here is some code I added in to make the data easier
@@ -86,19 +94,24 @@ public class MainActivity extends AppCompatActivity {
                         Log.v(TAG, "Transaction stored, value:" +
                                 myTransaction.getValue() + " ... ID:" + myTransaction.getId());
 
-                        listfrag.listItems.add("Transaction " + id + ": " + data);
+                        listfrag.listItems.add("Transaction " + id + ": $" + data);
                         listfrag.adapter.notifyDataSetChanged();
 
                         theIntent.putExtra("data",data);        //putting the data passed from the dialog view into the intent.
                         etView.setText(null);                   //erasing the data from the view
                         Toast.makeText(getApplicationContext(), //debugging and whatnot
-                                "the data is " + data, Toast.LENGTH_SHORT).show();
+                                "Purchase or Transaction of $ " + data + " added"
+                                , Toast.LENGTH_SHORT).show();
 
                         //Store count to SharedPreferences
                         count++;
                         editor.putInt("count", count);
                         editor.apply();
 
+                        //store total to sharedpreferences
+                        total += Float.parseFloat(data);
+                        editor.putFloat("data", total);
+                        editor.apply();
 
                     }
                 });
@@ -107,7 +120,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
 
-
+    //Launch a new activity when the user swipes right(Probably will be a summary page)
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        //Get swipe data
+        switch(touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+                if(x2 < x1){
+                    //Launch new page
+                    Intent i = new Intent(MainActivity.this, Summary.class);
+                    startActivity(i);
+                }
+                break;
+        }
+        return false;
     }
 }
---
+++ Fiscalitics/app/src/main/java/com/example/fiscalitics/Summary.java
@@ -0,0 +1,15 @@
+package com.example.fiscalitics;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Summary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+        //Animate lateral slide-in
+        overridePendingTransition(R.anim.enter, R.anim.exit);
+    }
+}
