8486fe8c8f3a044b71b75a6d265033a537c3aa67
Dante Coupet <dacoupet@gmail.com>
2020-11-28 15:24:17 -0500

    removed stuff

--- Fiscalitics/app/src/main/java/com/example/fiscalitics/Category.java
+++ Fiscalitics/app/src/main/java/com/example/fiscalitics/Category.java
@@ -6,15 +6,7 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
 
 public class Category extends AppCompatActivity {
 
@@ -34,45 +26,6 @@ public class Category extends AppCompatActivity {
         Button btnCar = (Button) findViewById(R.id.categoryCar);
         Button btnMisc = (Button) findViewById(R.id.categoryMisc);
 
-<<<<<<< HEAD
-=======
-
-        PieChart pieChart = new PieChart(this);
-
-        ArrayList NoOfEmp = new ArrayList();
-
-        NoOfEmp.add(new Entry(945f, 0));
-        NoOfEmp.add(new Entry(1040f, 1));
-        NoOfEmp.add(new Entry(1133f, 2));
-        NoOfEmp.add(new Entry(1240f, 3));
-        NoOfEmp.add(new Entry(1369f, 4));
-        NoOfEmp.add(new Entry(1487f, 5));
-        NoOfEmp.add(new Entry(1501f, 6));
-        NoOfEmp.add(new Entry(1645f, 7));
-        NoOfEmp.add(new Entry(1578f, 8));
-        NoOfEmp.add(new Entry(1695f, 9));
-        PieDataSet dataSet = new PieDataSet(NoOfEmp, "Number Of Employees");
-
-        ArrayList year = new ArrayList();
-
-        year.add("2008");
-        year.add("2009");
-        year.add("2010");
-        year.add("2011");
-        year.add("2012");
-        year.add("2013");
-        year.add("2014");
-        year.add("2015");
-        year.add("2016");
-        year.add("2017");
-        
-        PieData data = new PieData(year, dataSet);
-        pieChart.setData(data);
-        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        pieChart.animateXY(5000, 5000);
-
-
->>>>>>> 1089e892ebf957939ab27eb9f79265befda4bd52
         btnFood.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
