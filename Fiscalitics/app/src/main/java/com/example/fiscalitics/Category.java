02b4551a13a5c56f83c249f9076cdd1fd9a9759d
Dante Coupet <dacoupet@gmail.com>
2020-11-28 16:00:39 -0500

    added files for piechart fragment. lots of bugs to fix

--- Fiscalitics/app/src/main/java/com/example/fiscalitics/Category.java
+++ Fiscalitics/app/src/main/java/com/example/fiscalitics/Category.java
@@ -1,22 +1,37 @@
 package com.example.fiscalitics;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Category extends AppCompatActivity {
 
+    private static final String TAG = Category.class.getCanonicalName();
+
     float x1, x2, y1, y2;
+
+    PieChart pieChart;
+    PieData pieData;
+    List<PieEntry> pieEntryList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
         //Animate lateral slide-in
         overridePendingTransition(R.anim.enterl, R.anim.exitl);
 
@@ -26,40 +41,67 @@ public class Category extends AppCompatActivity {
         Button btnCar = (Button) findViewById(R.id.categoryCar);
         Button btnMisc = (Button) findViewById(R.id.categoryMisc);
 
+        final PieChartFragment piechartfrag = new PieChartFragment();
+        final FragmentTransaction trans = getSupportFragmentManager().beginTransaction();
+
+        pieChart = findViewById(R.id.pieChart);
+        //pieChart.setUsePercentValues(true);
+        pieEntryList.add(new PieEntry(10,"India"));
+        pieEntryList.add(new PieEntry(5,"US"));
+        pieEntryList.add(new PieEntry(7,"UK"));
+        pieEntryList.add(new PieEntry(3,"NZ"));
+        PieDataSet pieDataSet = new PieDataSet(pieEntryList,"country");
+        pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
+
+        trans.add(R.id.pieChartFragmentContainer,piechartfrag,TAG);
+
         btnFood.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (!trans.isEmpty()) trans.remove(piechartfrag);
+                trans.commit();
             }
         });
 
         btnEntertainment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+//                if (!trans.isEmpty()) trans.remove(piechartfrag);
+//                trans.commit();
             }
         });
 
         btnTravel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (!trans.isEmpty()) trans.remove(piechartfrag);
+                trans.commit();
             }
         });
 
         btnCar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+//                if (!trans.isEmpty()) trans.remove(piechartfrag);
+//                trans.commit();
             }
         });
 
         btnMisc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+//                if (!trans.isEmpty()) trans.remove(piechartfrag);
+//                trans.commit();
             }
         });
+
+
+    }
+
+    public void initPieChart() {
+
     }
 
     //Launch a main activity when the user swipes right
