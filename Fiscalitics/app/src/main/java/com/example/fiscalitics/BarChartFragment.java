1410ffdb775b9beb3d2418133d3d12c821106f88
Dante Coupet <dacoupet@gmail.com>
2020-11-29 02:01:03 -0500

    still tweaking the bar chart

--- Fiscalitics/app/src/main/java/com/example/fiscalitics/BarChartFragment.java
+++ Fiscalitics/app/src/main/java/com/example/fiscalitics/BarChartFragment.java
@@ -8,19 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,8 +20,6 @@ import java.util.List;
 public class BarChartFragment extends Fragment {
 
     private BarChart chart;
-    private SeekBar seekBarX, seekBarY;
-    private TextView tvX, tvY;
 
     public BarChartFragment() {
         // Required empty public constructor
@@ -38,7 +28,6 @@ public class BarChartFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.v("TAG","barchartfrag");
     }
 
     @Override
@@ -49,15 +38,14 @@ public class BarChartFragment extends Fragment {
 
         chart = view.findViewById(R.id.barChart);
         List<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(0f, 30f));
-        entries.add(new BarEntry(1f, 80f));
-        entries.add(new BarEntry(2f, 60f));
-        entries.add(new BarEntry(3f, 50f));
-        // gap of 2f
-        entries.add(new BarEntry(5f, 70f));
-        entries.add(new BarEntry(6f, 60f));
+        entries.add(new BarEntry(0f, 10f));
+        entries.add(new BarEntry(1f, 20f));
+        entries.add(new BarEntry(2f, 30f));
+        entries.add(new BarEntry(3f, 40f));
+        entries.add(new BarEntry(4f, 50f));
+        entries.add(new BarEntry(5f, 60f));
 
-        BarDataSet set = new BarDataSet(entries, "BarDataSet");
+        BarDataSet set = new BarDataSet(entries, "Expenditure");
 
         BarData data = new BarData(set);
         data.setBarWidth(0.9f); // set custom bar width
