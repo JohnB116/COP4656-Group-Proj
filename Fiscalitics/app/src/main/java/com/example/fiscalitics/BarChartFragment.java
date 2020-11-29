2b184c76221e4d22f168a46a87ea30c7fa3937b1
Dante Coupet <dacoupet@gmail.com>
2020-11-29 01:52:37 -0500

    GOT THE BAR CHART TO WORKKKKKKKK

--- Fiscalitics/app/src/main/java/com/example/fiscalitics/BarChartFragment.java
+++ Fiscalitics/app/src/main/java/com/example/fiscalitics/BarChartFragment.java
@@ -8,19 +8,28 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class BarChartFragment extends Fragment {
 
-    BarChart chart;
+    private BarChart chart;
+    private SeekBar seekBarX, seekBarY;
+    private TextView tvX, tvY;
 
     public BarChartFragment() {
         // Required empty public constructor
@@ -39,37 +48,22 @@ public class BarChartFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_bar_chart, container, false);
 
         chart = view.findViewById(R.id.barChart);
-        ArrayList NoOfEmp = new ArrayList();
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0f, 30f));
+        entries.add(new BarEntry(1f, 80f));
+        entries.add(new BarEntry(2f, 60f));
+        entries.add(new BarEntry(3f, 50f));
+        // gap of 2f
+        entries.add(new BarEntry(5f, 70f));
+        entries.add(new BarEntry(6f, 60f));
 
-        NoOfEmp.add(new BarEntry(945f, 0));
-        NoOfEmp.add(new BarEntry(1040f, 1));
-        NoOfEmp.add(new BarEntry(1133f, 2));
-        NoOfEmp.add(new BarEntry(1240f, 3));
-        NoOfEmp.add(new BarEntry(1369f, 4));
-        NoOfEmp.add(new BarEntry(1487f, 5));
-        NoOfEmp.add(new BarEntry(1501f, 6));
-        NoOfEmp.add(new BarEntry(1645f, 7));
-        NoOfEmp.add(new BarEntry(1578f, 8));
-        NoOfEmp.add(new BarEntry(1695f, 9));
+        BarDataSet set = new BarDataSet(entries, "BarDataSet");
 
-        ArrayList year = new ArrayList();
-
-        year.add("2008");
-        year.add("2009");
-        year.add("2010");
-        year.add("2011");
-        year.add("2012");
-        year.add("2013");
-        year.add("2014");
-        year.add("2015");
-        year.add("2016");
-        year.add("2017");
-
-        BarDataSet bardataset = new BarDataSet(NoOfEmp, "No Of Employee");
-        chart.animateY(5000);
-        BarData data = new BarData();
-        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        BarData data = new BarData(set);
+        data.setBarWidth(0.9f); // set custom bar width
         chart.setData(data);
+        chart.setFitBars(true); // make the x-axis fit exactly all bars
+        chart.invalidate(); // refresh
 
         return view;
     }
